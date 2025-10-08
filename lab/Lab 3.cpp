0ac0e34c3eaa8f48a08158765fdf8c85dd5ca342
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-10-08 16:22:01 +0700

    Update Lab 3.cpp

--- lab/Lab 3.cpp
+++ lab/Lab 3.cpp
@@ -1,11 +1,13 @@
-#include <stdio.h>
-#include <math.h>
+#include <stdio.h> 
+#include <stdlib.h> e
 
+ะ
 int isPrime(int num) {
     if (num < 2) {
-        return 0;
+        return 0; 
     }
-    for (int i = 2; i * i <= num; i++) { 
+  
+    for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) {
             return 0;
         }
@@ -15,41 +17,44 @@ int isPrime(int num) {
 
 int main() {
     int n;
-    printf("Enter N : ");
-    scanf("%d", &n);
-
-    if (n <= 0) {
-        printf("N must be a positive integer.\n");
-        return 1; 
+    printf("Enter N (size of array): ");
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("Invalid input for N. Exiting.\n");
+        return 1;
     }
 
-    int *arr = (int *)malloc(n * sizeof(int));
+    int *arr = malloc(n * sizeof(int));
 
     if (arr == NULL) {
-        printf("Memory allocation failed!\n");
+        printf("Memory allocation failed. Exiting.\n");
         return 1;
     }
 
     for (int i = 0; i < n; i++) {
         printf("Enter value[%d] : ", i);
-        scanf("%d", &arr[i]);
+        if (scanf("%d", &arr[i]) != 1) {
+            printf("Invalid input. Exiting.\n");
+            free(arr);
+            return 1;
+        }
     }
 
-    printf("Index:  ");
+    printf("\nIndex:  ");
     for (int i = 0; i < n; i++) {
-        printf("%2d ", i);
+        printf("%3d", i);
     }
     printf("\n");
 
     printf("Array:  ");
     for (int i = 0; i < n; i++) {
         if (isPrime(arr[i])) {
-            printf("%2d ", arr[i]);
+            printf("%3d", arr[i]);
         } else {
-            printf("%2s ", "#");
+            printf("%3s", "#");
         }
     }
     printf("\n");
+
     free(arr);
 
     return 0;
