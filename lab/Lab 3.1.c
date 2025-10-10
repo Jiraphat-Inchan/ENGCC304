6d75bde51786c9917ada2b56594a3808f6290b29
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-10-10 16:28:24 +0700

    Add files via upload

---
+++ lab/Lab 3.1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    float H, W;
+    float Area;
+
+    printf("H: ");
+
+    scanf("%f", &H);
+
+    printf("W: ");
+ 
+    scanf("%f", &W);
+
+    Area = (H * W) / 2.0; 
+
+    printf("Area = %.1f\n", Area);
+
+    return 0; 
+}
